dc9b14be72dc8312014a2788fe53d23e40573c65
Franklin Ramírez <framirez@estrategiayseguridad.com>
2021-07-01 20:03:38 -0600

    testing new configs

--- custom_tiles/tiles/mayan-calendar.tsx
+++
@@ -1,7 +0,0 @@
-<?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.5" tiledversion="1.7.0" name="mayan-calendar" tilewidth="32" tileheight="32" tilecount="961" columns="31">
- <properties>
-  <property name="collides" type="bool" value="true"/>
- </properties>
- <image source="../images/mayan-calendar.png" width="1000" height="994"/>
-</tileset>
